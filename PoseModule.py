82b970175cea3145705e6519fb7eeff72a909985
Sumon <abusumon1701@gmail.com>
2025-06-27 16:04:36 +0600

    Initial commit: HandTrackingModule and PoseModule

---
+++ PoseModule.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+class poseDetector():
+
+    def __init__(self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        
+        self.mpDraw =mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+    
+    def findPosition(self, img, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(img, (cx, cy), 5, (255, 0, 0), cv.FILLED)
+        return lmList
+
+
+def main():
+    cap = cv.VideoCapture(0)
+    pTime = 0
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img)
+
+        if len(lmList) != 0:
+            cv.circle(img, (lmList[0][1], lmList[0][2]), 15, (255, 0, 255), cv.FILLED)
+
+        cTime = time.time()
+        fps = 1/ (cTime - pTime)
+        pTime = cTime
+
+        cv.putText(img, str(int(fps)), (70, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        cv.imshow("Image", img)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+
+if __name__ == "__main__":
+    main()
